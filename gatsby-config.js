ed8fdc878aaf3c8e60db893f29a2320f5cd10013
zegheim <justinarista@gmail.com>
2020-08-01 10:32:16 +0700

    add site description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `PT. TRIROYAL TIMURRAYA`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Palm Oil Mill Contractor | Water Treatment`,
     author: `@zegheim`,
   },
   plugins: [
