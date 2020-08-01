8977faf9b136c91ad27202178972b1a9d85bd166
zegheim <justinarista@gmail.com>
2020-08-01 10:28:40 +0700

    add header image to header

--- src/components/image.js
+++ src/components/image.js
@@ -13,20 +13,21 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const Image = () => {
+const HeaderImage = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      headerImage: file(relativePath: { eq: "header.png" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 800, quality: 100) {
             ...GatsbyImageSharpFluid
+            ...GatsbyImageSharpFluidLimitPresentationSize
           }
         }
       }
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return <Img fluid={data.headerImage.childImageSharp.fluid} />
 }
 
-export default Image
+export default HeaderImage
