0123d46a4613adcf9a2d3be3e653f9ad32de1ee1
zegheim <justinarista@gmail.com>
2020-08-01 16:18:27 +0700

    use bulma scss, remove inline styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,13 @@
 import { Link } from "gatsby"
 import React from "react"
 
-import HeaderImage from "../components/image"
+import HeaderImage from "@components/image"
 
 const Header = () => (
-  <header
-    style={{
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <Link to="/">
-        <HeaderImage />
-      </Link>
-    </div>
+  <header>
+    <Link to="/">
+      <HeaderImage />
+    </Link>
   </header>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "@components/layout"
+import SEO from "@components/seo"
+
+import "@components/styles.scss"
 
 const LandingPage = () => (
   <Layout>
