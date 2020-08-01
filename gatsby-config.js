ad1faf0d915ef32d34edb3bf88c53383c2240328
zegheim <justinarista@gmail.com>
2020-08-01 15:11:34 +0700

    add alias for imports, use bulma

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,9 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,8 +16,18 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-alias-imports`,
+      options: {
+        alias: {
+          "@src": "src",
+          "@components": "src/components",
+          "@pages": "src/pages",
+          "@images": "src/images",
+        },
+        extensions: ["js"],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
