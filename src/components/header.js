388dbbd5feddb645d68d64cda96a4d44528374a4
zegheim <justinarista@gmail.com>
2020-08-01 20:44:57 +0700

    use inline image

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,38 @@
 import { Link } from "gatsby"
 import React from "react"
 
-import HeaderImage from "@components/image"
+import header from "@images/header.png"
 
 const Header = () => (
   <header>
-    <div className="container centered">
+    <div className="container px-6 py-6 has-text-centered">
       <Link to="/">
-        <HeaderImage />
+        <img src={header} className="responsive" />
       </Link>
+    </div>
+    <div className="container px-6">
+      <div className="tabs is-centered">
+        <ul>
+          <li>
+            <a>beranda</a>
+          </li>
+          <li>
+            <a>beranda</a>
+          </li>
+          <li>
+            <a>beranda</a>
+          </li>
+          <li>
+            <a>beranda</a>
+          </li>
+          <li>
+            <a>beranda</a>
+          </li>
+          <li>
+            <a>beranda</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </header>
 )
