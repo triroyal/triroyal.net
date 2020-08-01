88cb2f2f6db1a604911f1d532ccf040e26847b4c
zegheim <justinarista@gmail.com>
2020-08-01 23:07:29 +0700

    finished footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,28 @@
 import React from "react"
 
+const address =
+  "Jln. Pulau Sangir Talaud I No. 8, Kawasan Industri Modern IV, Deli Serdang, Indonesia 20374"
+const mobileNums = ["+62-852-7791-8325", "+62-852-9889-0246"]
+const emailAddrs = ["mail@triroyal.net", "triroyal@yahoo.co.id"]
+
 const Footer = () => (
-  <footer className="footer">
-    <div className="content has-text-centered">
-      <p>
-        Jln. Pulau Sangir Talaud I No. 8, Kawasan Industri Modern IV, Deli
-        Serdang, Indonesia 20374
-      </p>
-      <p>
-        <strong>Mobile: </strong> +62-852-7791-8325 | +62-852-9889-0246
-      </p>
-      <p>
-        <strong>E-mail: </strong>{" "}
-        <a href="mailto:mail@triroyal.net">mail@triroyal.net</a> |{" "}
-        <a href="mailto:triroyal@yahoo.co.id">triroyal@yahoo.co.id</a>
+  <footer className="footer has-background-primary">
+    <div className="content has-text-centered has-text-white">
+      <p className="mb-2">{address}</p>
+      <p className="mb-2">Mobile: {mobileNums.join(" | ")} </p>
+      <p className="mb-2">
+        E-mail:{" "}
+        {emailAddrs
+          .map(e => (
+            <a className="has-text-info" href={`mailto:${e}`}>
+              {e}
+            </a>
+          ))
+          .reduce((acc, e) => (
+            <>
+              {acc} | {e}
+            </>
+          ))}
       </p>
       <p>Copyright 1994-{new Date().getFullYear()} © PT. Triroyal Timurraya</p>
     </div>
