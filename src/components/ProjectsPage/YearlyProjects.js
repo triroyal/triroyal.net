4b200109db367b53b603b5395f14b325e83f7e35
zegheim <justinarista@gmail.com>
2020-08-02 22:19:52 +0700

    fixed project card appearance

--- src/components/ProjectsPage/YearlyProjects.js
+++ src/components/ProjectsPage/YearlyProjects.js
@@ -1,18 +1,20 @@
 import React from "react"
 
 const ProjectCard = ({ client, description, location }) => (
-  <div className="column card is-one-third">
-    <header className="card-header">
-      <p className="card-header-title">{client}</p>
-    </header>
-    <div className="card-content">
-      <div className="content">
-        <p className="justify">
-          <strong>Pekerjaan:</strong> {description}
-        </p>
-        <p className="justify">
-          <strong>Lokasi:</strong> {location}
-        </p>
+  <div className="column is-one-third">
+    <div className="card full-height">
+      <header className="card-header">
+        <p className="card-header-title">{client}</p>
+      </header>
+      <div className="card-content">
+        <div className="content">
+          <p className="justify">
+            <strong>Pekerjaan:</strong> {description}
+          </p>
+          <p className="justify">
+            <strong>Lokasi:</strong> {location}
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -21,7 +23,7 @@ const ProjectCard = ({ client, description, location }) => (
 const YearlyProjects = ({ year, projectsList }) => (
   <div className="centered vertical pb-5">
     <h3 className="title is-3">{year}</h3>
-    <div className="columns is-multiline align-image">
+    <div className="columns is-multiline align-image full-width">
       {projectsList.map((p) => (
         <ProjectCard
           key={`${p.client}-${p.description}-${p.location}`}
