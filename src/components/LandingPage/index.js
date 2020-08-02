44329d175d74ffa257794284e7fd4ad6f297b512
zegheim <justinarista@gmail.com>
2020-08-02 16:19:49 +0700

    finishied landing page

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,10 +1,25 @@
 import React from "react"
+import landing from "@images/landing.png"
 
 const LandingPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+  <div className="container px-6 pt-4 pb-6">
+    <div className="has-text-centered pb-4">
+      <img
+        src={landing}
+        className="responsive"
+        alt="Triroyal engineering team."
+      />
+    </div>
+    <div className="centered">
+      <p className="align-image justify">
+        Selamat datang ke PT. Triroyal Timurraya. Kami adalah perusahaan yang
+        bergerak di bidang jasa konstruksi sipil dan mekanikal yang berlokasi di
+        Sumatera Utara, Indonesia. Kami melayani berbagai jenis permintaan
+        konstruksi mulai dari fabrikasi dan instalasi rebusan/sterilizer, sistem
+        water treatment, pembangunan tangki timbun, bulking station sampai
+        pembangunan Pabrik Kelapa Sawit (PKS) sepenuhnya.
+      </p>
+    </div>
   </div>
 )
 
