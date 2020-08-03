f591ebae4ffb3c5b7671aeae03956c4e7b7e4770
zegheim <justinarista@gmail.com>
2020-08-03 21:06:32 +0700

    fix footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,33 +1,37 @@
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faEnvelope,
+  faMap,
+  faPhoneAlt,
+} from "@fortawesome/free-solid-svg-icons"
+import { faCopyright } from "@fortawesome/free-regular-svg-icons"
 
 const address =
   "Jln. Pulau Sangir Talaud I No. 8, Kawasan Industri Modern IV, Deli Serdang, Indonesia 20374"
 const mobileNums = ["+62-852-7791-8325", "+62-852-9889-0246"]
-const emailAddrs = ["mail@triroyal.net", "triroyal@yahoo.co.id"]
+const emailAddr = "mail@triroyal.net"
+const copyright = `1994-${new Date().getFullYear()} PT. Triroyal Timurraya`
 
+const FooterLine = ({ icon, content }) => (
+  <p className="subtitle is-6 has-text-white">
+    <FontAwesomeIcon icon={icon} /> {content}
+  </p>
+)
 const Footer = () => (
-  <footer className="footer has-background-primary px-5 py-6">
-    <div className="content has-text-centered has-text-white">
-      <p className="mb-2">{address}</p>
-      <p className="mb-2">
-        <strong className="has-text-white">Mobile:</strong>{" "}
-        {mobileNums.join(" | ")}{" "}
-      </p>
-      <p className="mb-2">
-        <strong className="has-text-white">Email:</strong>{" "}
-        {emailAddrs
-          .map((e) => (
-            <a className="has-text-info" href={`mailto:${e}`}>
-              {e}
-            </a>
-          ))
-          .reduce((acc, e) => (
-            <>
-              {acc} | {e}
-            </>
-          ))}
-      </p>
-      <p>Copyright 1994-{new Date().getFullYear()} © PT. Triroyal Timurraya</p>
+  <footer className="footer has-background-primary px-5 py-5">
+    <div className="content has-text-centered">
+      <FooterLine icon={faMap} content={address} />
+      <FooterLine icon={faPhoneAlt} content={mobileNums.join(" | ")} />
+      <FooterLine
+        icon={faEnvelope}
+        content={
+          <a className="has-text-warning" href={`mailto:${emailAddr}`}>
+            {emailAddr}
+          </a>
+        }
+      />
+      <FooterLine icon={faCopyright} content={copyright} />
     </div>
   </footer>
 )
