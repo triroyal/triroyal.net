1013e3a30eef0ceed40518a5d16179488ec34246
zegheim <justinarista@gmail.com>
2020-08-03 19:19:44 +0700

    refactored to use less nested divs

--- src/components/AboutPage/index.js
+++ src/components/AboutPage/index.js
@@ -1,33 +1,31 @@
 import React from "react"
-import about from "@images/about.png"
+import About from "@images/about.png"
 
 const AboutPage = () => (
-  <div className="container px-5 pb-5">
-    <div className="has-text-centered pb-4">
+  <div className="container centered vertical px-5 pb-5">
+    <div className="pb-4">
       <img
-        src={about}
+        src={About}
         className="responsive"
         alt="Triroyal engineering team."
       />
     </div>
-    <div className="centered vertical">
-      <p className="align-image justify pb-4">
-        PT. Triroyal Timurraya merupakan perusahaan swasta nasional yang
-        didirikan oleh R.Kasin di Medan, Sumatera Utara pada tahun 1994.
-        Perusahaan ini pada awalnya bergerak dalam bidang usaha Heavy Metal
-        Construction dan Water Treatment Plant yang kemudian dan terus
-        berkembang sebagai perusahaan konstruksi sipil, mekanikal, dan
-        elektrikal untuk pembangunan Pabrik Kelapa Sawit (PKS).
-      </p>
-      <p className="align-image justify">
-        Sesuai dengan visi dan misi perusahaan yang didukung dengan pengalaman,
-        fasilitas dan juga keahlian serta dedikasi sumber daya manusia (SDM),
-        PT. Triroyal Timurraya selalu berusaha memenuhi standar mutu yang
-        diinginkan klien. Dalam usaha menjaga kebersihan lingkungan, PT.
-        Triroyal Timurraya bekerja sama dengan Aquakimia Sdn. Bhd. Malaysia
-        dalam bidang Environmental Protection.
-      </p>
-    </div>
+    <p className="align-image justify pb-4">
+      PT. Triroyal Timurraya merupakan perusahaan swasta nasional yang didirikan
+      oleh R.Kasin di Medan, Sumatera Utara pada tahun 1994. Perusahaan ini pada
+      awalnya bergerak dalam bidang usaha Heavy Metal Construction dan Water
+      Treatment Plant yang kemudian dan terus berkembang sebagai perusahaan
+      konstruksi sipil, mekanikal, dan elektrikal untuk pembangunan Pabrik
+      Kelapa Sawit (PKS).
+    </p>
+    <p className="align-image justify">
+      Sesuai dengan visi dan misi perusahaan yang didukung dengan pengalaman,
+      fasilitas dan juga keahlian serta dedikasi sumber daya manusia (SDM), PT.
+      Triroyal Timurraya selalu berusaha memenuhi standar mutu yang diinginkan
+      klien. Dalam usaha menjaga kebersihan lingkungan, PT. Triroyal Timurraya
+      bekerja sama dengan Aquakimia Sdn. Bhd. Malaysia dalam bidang
+      Environmental Protection.
+    </p>
   </div>
 )
 
--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,25 +1,23 @@
 import React from "react"
-import landing from "@images/about.png"
+import Landing from "@images/about.png"
 
 const LandingPage = () => (
-  <div className="container px-5 pb-5">
-    <div className="has-text-centered pb-4">
+  <div className="container centered vertical px-5 pb-5">
+    <div className="pb-4">
       <img
-        src={landing}
+        src={Landing}
         className="responsive"
         alt="Triroyal engineering team."
       />
     </div>
-    <div className="centered">
-      <p className="align-image justify">
-        Selamat datang ke PT. Triroyal Timurraya. Kami adalah perusahaan yang
-        bergerak di bidang jasa konstruksi sipil dan mekanikal yang berlokasi di
-        Sumatera Utara, Indonesia. Kami melayani berbagai jenis permintaan
-        konstruksi mulai dari fabrikasi dan instalasi rebusan/sterilizer, sistem
-        water treatment, pembangunan tangki timbun, bulking station sampai
-        pembangunan Pabrik Kelapa Sawit (PKS) sepenuhnya.
-      </p>
-    </div>
+    <p className="align-image justify">
+      Selamat datang ke PT. Triroyal Timurraya. Kami adalah perusahaan yang
+      bergerak di bidang jasa konstruksi sipil dan mekanikal yang berlokasi di
+      Sumatera Utara, Indonesia. Kami melayani berbagai jenis permintaan
+      konstruksi mulai dari fabrikasi dan instalasi rebusan/sterilizer, sistem
+      water treatment, pembangunan tangki timbun, bulking station sampai
+      pembangunan Pabrik Kelapa Sawit (PKS) sepenuhnya.
+    </p>
   </div>
 )
 
