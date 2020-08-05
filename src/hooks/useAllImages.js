246d70cec48bd272507345dc1846ae0488b53184
zegheim <justinarista@gmail.com>
2020-08-05 12:39:11 +0700

    minor formatting

--- src/hooks/useAllImages.js
+++ src/hooks/useAllImages.js
@@ -1,18 +1,20 @@
 import { useStaticQuery, graphql } from "gatsby"
 
 export const useAllImages = () => {
-  const { allCloudinaryMedia } = useStaticQuery(graphql`
-    query {
-      allCloudinaryMedia {
-        edges {
-          node {
-            public_id
-            secure_url
+  const { allCloudinaryMedia } = useStaticQuery(
+    graphql`
+      query {
+        allCloudinaryMedia {
+          edges {
+            node {
+              public_id
+              secure_url
+            }
           }
         }
       }
-    }
-  `)
+    `
+  )
 
   return allCloudinaryMedia.edges.map((e) => e.node)
 }
